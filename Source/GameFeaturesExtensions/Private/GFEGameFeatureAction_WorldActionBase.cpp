24dee8deb3d7b7c15596a9fab243c0afcca86497
michael.delva <michael@emidee.net>
2022-02-15 17:21:27 +0100

    Added WorldActionBase action

---
+++ Source/GameFeaturesExtensions/Private/GFEGameFeatureAction_WorldActionBase.cpp
@@ -0,0 +1,24 @@
+#include "GFEGameFeatureAction_WorldActionBase.h"
+
+void UGFEGameFeatureAction_WorldActionBase::OnGameFeatureActivating()
+{
+    GameInstanceStartHandle = FWorldDelegates::OnStartGameInstance.AddUObject( this, &UGFEGameFeatureAction_WorldActionBase::HandleGameInstanceStart );
+
+    for ( const FWorldContext & world_context : GEngine->GetWorldContexts() )
+    {
+        AddToWorld( world_context );
+    }
+}
+
+void UGFEGameFeatureAction_WorldActionBase::OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context )
+{
+    FWorldDelegates::OnStartGameInstance.Remove( GameInstanceStartHandle );
+}
+
+void UGFEGameFeatureAction_WorldActionBase::HandleGameInstanceStart( UGameInstance * game_instance )
+{
+    if (const FWorldContext * world_context = game_instance->GetWorldContext() )
+    {
+        AddToWorld( *world_context );
+    }
+}
---
+++ Source/GameFeaturesExtensions/Public/GFEGameFeatureAction_WorldActionBase.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <GameFeatureAction.h>
+
+#include "GFEGameFeatureAction_WorldActionBase.generated.h"
+
+// This is a copy / paste from the one you can find in the ValleyOfTheAncient UE5 sample
+UCLASS( Abstract )
+class GAMEFEATURESEXTENSIONS_API UGFEGameFeatureAction_WorldActionBase : public UGameFeatureAction
+{
+    GENERATED_BODY()
+
+public:
+    void OnGameFeatureActivating() override;
+    void OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context ) override;
+
+private:
+
+    void HandleGameInstanceStart( UGameInstance * game_instance );
+    virtual void AddToWorld( const FWorldContext & world_context ) PURE_VIRTUAL( UGFEGameFeatureAction_WorldActionBase::AddToWorld, );
+
+    FDelegateHandle GameInstanceStartHandle;
+};
